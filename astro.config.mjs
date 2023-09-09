5db929da77df9de30eb358a7920f6630b41d4eae
Joaquín Blanco <100656622+joackob@users.noreply.github.com>
2023-09-09 16:03:26 -0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
