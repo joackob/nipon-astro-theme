5c60390b9534a0a8bf5cd873abe903e6ffe63c4a
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-09-12 02:44:08 -0300

    agregue varios nuevos iconos

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Joacko Blog";
-export const SITE_DESCRIPTION =
-  "Ingeniería de Software, Ciencia de datos, Docencia y Programación";
+export const SITE_TITLE = "Your History";
+export const SITE_DESCRIPTION = "A template for writters";
 export const SITE_URL = "";
